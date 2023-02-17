871d4a8e51a1902048dbc51a2319a1168f800586
Alvin Okafor <alvin2k99@gmail.com>
2023-02-17 23:04:06 +0100

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: 1, text: 'Learn HTML', done: true },
+  { id: 2, text: 'Learn JavaScript', done: true },
+  { id: 3, text: 'Learn Vue', done: false }
+])
+
+function addTodo() {
+  todos.value.push({ id: 4, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  // const idx = todos.value.findIndex(todo => id === todo.id)
+  // todos.value.splice(idx, 1)
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
+function toggleCompleted() {
+  hideCompleted.value = !hideCompleted.value
+}
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter(todo => !todo.done)
+    : todos.value
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button class="bg-slate-700" @click="toggleCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.done {
+  text-decoration: line-through;
 }
 </style>
